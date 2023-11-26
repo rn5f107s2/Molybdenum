cee4b715ee7ab837de4b3c92f645cc415223ae28
rn5f107s2 <clemens.lerchl@gmail.com>
2023-11-26 21:59:24 +0100

    Now works with clarity net

--- src/nnue.cpp
+++ src/nnue.cpp
@@ -5,18 +5,6 @@
 #include <cstring>
 #include <fstream>
 
-const int INPUT_SIZE = 12 * 64;
-const int L1_SIZE = 32;
-const int OUTPUT_SIZE = 1;
-const int NET_SIZE = 3;
-
-const std::array<int, NET_SIZE> LAYER_SIZE = {INPUT_SIZE, L1_SIZE, OUTPUT_SIZE};
-std::array<int16_t , L1_SIZE * INPUT_SIZE> weights0;
-std::array<int16_t, L1_SIZE * OUTPUT_SIZE * 2> weights1;
-std::array<int16_t, L1_SIZE> bias0;
-std::array<int16_t, OUTPUT_SIZE> bias1;
-std::array<int16_t, L1_SIZE> accumulator;
-
 void readNetwork(const std::string &filename) {
     std::ifstream stream{filename, std::ios::binary};
 
@@ -27,10 +15,8 @@ void readNetwork(const std::string &filename) {
 }
 
 void initAccumulator(std::array<u64, 13> &bitboards) {
-    std::array<std::array<int16_t, L1_SIZE>, 2> l1{};
-    memset(&accumulator, 0, sizeof(accumulator[0]) * accumulator.size());
-    memccpy(&l1[WHITE], &bias0[0], sizeof(int16_t), L1_SIZE / 2);
-    memccpy(&l1[BLACK], &bias0  [L1_SIZE / 2], sizeof(int16_t), L1_SIZE / 2);
+    memcpy(&accumulator[WHITE], &bias0[0], sizeof(int16_t) * L1_SIZE);
+    memcpy(&accumulator[BLACK], &bias0[0], sizeof(int16_t) * L1_SIZE);
 
     for (int pc = WHITE_PAWN; pc != NO_PIECE; pc++) {
         u64 pieceBB = bitboards[pc];
@@ -38,14 +24,20 @@ void initAccumulator(std::array<u64, 13> &bitboards) {
         while (pieceBB) {
             int square = popLSB(pieceBB);
 
-            for (int n = 0; n != L1_SIZE; n++)
-                l1[pc > WHITE_KING][n] += weights0[pc * square];
+            toggleFeature<On>(pc, square);
         }
     }
 
-    for (int w = 0; w != L1_SIZE / 2; w++) {
-        accumulator[WHITE] += relu(l1[WHITE][w]) * weights1[w];
+    std::cout << (calculate(WHITE)) * 400 / 16320 << "\n";
+}
+
+int calculate(Color c) {
+    int output = 0;
+
+    for (int n = 0; n != L1_SIZE; n++) {
+         output += relu(accumulator[ c][n]) * weights1[n          ];
+         output += relu(accumulator[!c][n]) * weights1[n + L1_SIZE];
     }
 
-    std::cout << ((accumulator[WHITE] - accumulator[BLACK]) + bias1[0]) * 400 / (255 * 64);
+    return output + bias1[0];
 }
