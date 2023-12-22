1b7230ca8770da3478c48afef61732960b2e05ad
rn5f107s2 <clemens.lerchl@gmail.com>
2023-12-22 19:59:40 +0100

    Fix time losses that occured if (increment / 2) was bigger tah time left

--- src/timemanagment.cpp
+++ src/timemanagment.cpp
@@ -3,7 +3,7 @@
 
 searchTime calcThinkingTime(int timeLeft, int increment, int movesToGo) {
     searchTime st;
-    int normalTime = (timeLeft / 20) + (increment / 2);
+    int normalTime = std::min((timeLeft / 20) + (increment / 2), timeLeft / 3);
     int mtgTime = (timeLeft / movesToGo);
     st.thinkingTime = std::chrono::milliseconds(std::max(normalTime, mtgTime) - moveOverHead);
     return st;
