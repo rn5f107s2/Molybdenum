1a7fea19e7441857c96189966fef96c02dabdc8f
rn5f107s2 <clemens.lerchl@gmail.com>
2024-01-04 21:36:25 +0100

    Reduce depth by 1 per default in LMR bench 4557828

--- src/search.h
+++ src/search.h
@@ -44,7 +44,7 @@ inline std::array<double, 256> initReductions() {
 static std::array<double, 256> Log = initReductions();
 
 inline int lmrReduction(int depth, int movecount, bool improving) {
-    return int(0.75 + !improving * 0.55 + Log[depth] * Log[movecount] / 2.19);
+    return int(0.65 + !improving * 0.45 + Log[depth] * Log[movecount] / 2.39);
 }
 
 inline int mateInPlies(int score) {
