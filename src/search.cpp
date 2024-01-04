ccd027109a074501290372f7610b64315615bbe8
rn5f107s2 <clemens.lerchl@gmail.com>
2024-01-04 21:08:08 +0100

    Reduce depth by 1 per default in LMR bench 2768174

--- src/search.cpp
+++ src/search.cpp
@@ -256,12 +256,12 @@ int search(int alpha, int beta, Position &pos, int depth, SearchInfo &si, Search
         moveCount++;
 
         reductions -= PvNode;
-        reductions = std::max(reductions, 1);
+        reductions = std::max(reductions, 0);
 
         if (depth > 2 && moveCount > 2) {
-            score = -search<NonPvNode>(-alpha - 1, -alpha, pos, depth - reductions, si, stack+1);
+            score = -search<NonPvNode>(-alpha - 1, -alpha, pos, depth - 1 - reductions, si, stack+1);
 
-            if (!PvNode && score > alpha && reductions > 1)
+            if (!PvNode && score > alpha && reductions > 0)
                 score = -search<NonPvNode>(-alpha - 1, -alpha, pos, depth - 1, si, stack+1);
 
             if (PvNode && score > alpha && score < beta)
