a614d4e72f051a1ccc3ed85ead44a77bca1e8336
rn5f107s2 <clemens.lerchl@gmail.com>
2024-01-05 13:17:20 +0100

    Fix#2 bench 10880936

--- src/timemanagment.cpp
+++ src/timemanagment.cpp
@@ -1,12 +1,14 @@
 #include <algorithm>
+#include <iostream>
 #include "timemanagement.h"
 
 searchTime calcThinkingTime(int timeLeft, int increment, int movesToGo) {
     searchTime st;
     int normalHardTime = std::min((timeLeft / 20) + (increment / 2), timeLeft / 3);
-    int normalSoftTime = std::min((timeLeft / 15) + (increment / 3), timeLeft / 3);
+    int normalSoftTime = std::min((timeLeft / 25) + (increment / 3), timeLeft / 3);
     int mtgTime = (timeLeft / movesToGo);
     st.thinkingTime[Hard] = std::chrono::milliseconds(std::max(normalHardTime, mtgTime) - moveOverHead);
     st.thinkingTime[Soft] = std::chrono::milliseconds(std::max(normalSoftTime, mtgTime) - moveOverHead);
+    std::cout << std::max(normalHardTime, mtgTime) - moveOverHead << " " << std::max(normalSoftTime, mtgTime) - moveOverHead << "\n";
     return st;
 }
