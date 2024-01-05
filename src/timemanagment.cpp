9af312489fc3bf8a650a34cbb9164aaad498f082
rn5f107s2 <clemens.lerchl@gmail.com>
2024-01-05 13:07:10 +0100

    Fix messed up order bench 10880936

--- src/timemanagment.cpp
+++ src/timemanagment.cpp
@@ -6,7 +6,7 @@ searchTime calcThinkingTime(int timeLeft, int increment, int movesToGo) {
     int normalHardTime = std::min((timeLeft / 20) + (increment / 2), timeLeft / 3);
     int normalSoftTime = std::min((timeLeft / 15) + (increment / 3), timeLeft / 3);
     int mtgTime = (timeLeft / movesToGo);
-    st.thinkingTime[Hard] = std::chrono::milliseconds(std::max(normalSoftTime, mtgTime) - moveOverHead);
-    st.thinkingTime[Soft] = std::chrono::milliseconds(std::max(normalHardTime, mtgTime) - moveOverHead);
+    st.thinkingTime[Hard] = std::chrono::milliseconds(std::max(normalHardTime, mtgTime) - moveOverHead);
+    st.thinkingTime[Soft] = std::chrono::milliseconds(std::max(normalSoftTime, mtgTime) - moveOverHead);
     return st;
 }
