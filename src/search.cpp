6c0a2736e857f483b625af9a5e5581b31e5fe74d
rn5f107s2 <clemens.lerchl@gmail.com>
2024-01-31 21:10:19 +0100

    bench 5275763

--- src/search.cpp
+++ src/search.cpp
@@ -285,6 +285,8 @@ int search(int alpha, int beta, Position &pos, int depth, SearchInfo &si, Search
 
             if (score < singBeta)
                 extensions = 1;
+            else if (!PvNode && singBeta >= beta)
+                return beta;
         }
 
         u64 prefetchKey = key;
