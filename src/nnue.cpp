5402ee2f4a1e6426286920b82e9a618cda32a717
rn5f107s2 <clemens.lerchl@gmail.com>
2024-02-13 16:28:56 +0100

    bench 9630572

--- src/nnue.cpp
+++ src/nnue.cpp
@@ -19,9 +19,9 @@
 #endif
 
 #ifdef MAKEFILE
-#define defaultNetPath "src/Nets/ne3s23p6.nnue"
+#define defaultNetPath "src/Nets/20240213.nnue"
 #else
-#define defaultNetPath "../src/Nets/ne3s23p6.nnue"
+#define defaultNetPath "../src/Nets/20240213.nnue"
 #endif
 
 
