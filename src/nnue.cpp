995bd2b076ff61b9d1dbe78cfb1006a49a6dac77
rn5f107s2 <clemens.lerchl@gmail.com>
2024-03-23 07:57:10 +0100

    bench 5884580

--- src/nnue.cpp
+++ src/nnue.cpp
@@ -18,7 +18,7 @@
 #undef PUSHED_MACRO
 #endif
 
-#define defaultNetPath "src/Nets/ne3s23p6.nnue"
+#define defaultNetPath "src/Nets/20240323.nnue"
 
 INCBIN(network, defaultNetPath);
 const Weights defaultWeights = *reinterpret_cast<const Weights*>(gnetworkData);
