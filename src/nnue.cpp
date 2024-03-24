c6dbe4695a3a77590a47152653626e2d4b25d460
rn5f107s2 <121191597+rn5f107s2@users.noreply.github.com>
2024-03-24 16:08:24 +0100

    New net using an additional 40m fens (#71)
    Elo   | 21.05 +- 10.32 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=8MB
    LLR   | 2.91 (-2.25, 2.89) [0.00, 5.00]
    Games | N: 2396 W: 730 L: 585 D: 1081
    Penta | [45, 246, 492, 349, 66]
    http://aytchell.eu.pythonanywhere.com/test/271/
    
    bench 8145294

--- src/nnue.cpp
+++ src/nnue.cpp
@@ -18,7 +18,7 @@
 #undef PUSHED_MACRO
 #endif
 
-#define defaultNetPath "src/Nets/20240323_128.nnue"
+#define defaultNetPath "src/Nets/20240324.nnue"
 
 INCBIN(network, defaultNetPath);
 const Weights defaultWeights = *reinterpret_cast<const Weights*>(gnetworkData);
