dfbf920984647505a819a97fa68b7feca6df7901
rn5f107s2 <clemens.lerchl@gmail.com>
2024-04-13 16:21:40 +0200

    bench 8626983

--- src/nnue.cpp
+++ src/nnue.cpp
@@ -18,7 +18,7 @@
 #undef PUSHED_MACRO
 #endif
 
-#define defaultNetPath "src/Nets/legacy12TScrelu-epoch40.bin"
+#define defaultNetPath "src/Nets/maininitSCReLU-40.bin"
 
 INCBIN(network, defaultNetPath);
 const Weights defaultWeights = *reinterpret_cast<const Weights*>(gnetworkData);
