531c4815da369d71a03b71b6aebd153398781414
rn5f107s2 <clemens.lerchl@gmail.com>
2024-04-15 13:21:28 +0200

    Increase the Hiddenlayer to 256
    Passed STC:
    Elo   | 22.39 +- 11.22 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=8MB
    LLR   | 2.89 (-2.25, 2.89) [0.00, 5.00]
    Games | N: 2020 W: 617 L: 487 D: 916
    Penta | [24, 192, 456, 306, 32]
    http://aytchell.eu.pythonanywhere.com/test/338/
    
    bench 6010245

--- src/nnue.h
+++ src/nnue.h
@@ -13,7 +13,7 @@ enum Toggle {
 };
 
 static const int INPUT_SIZE = 12 * 64;
-static const int L1_SIZE = 128;
+static const int L1_SIZE = 256;
 static const int OUTPUT_SIZE = 1;
 static const int NET_SIZE = 3;
 static const std::array<int, NET_SIZE> LAYER_SIZE = {INPUT_SIZE, L1_SIZE, OUTPUT_SIZE};
