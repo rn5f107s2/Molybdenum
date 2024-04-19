1a79a6bb6c344e6b6e7d63ee493b20d3d024f5eb
rn5f107s2 <121191597+rn5f107s2@users.noreply.github.com>
2024-04-19 18:24:45 +0200

    Add another 13m fens (#79)
    Fens generated using a special branch (https://github.com/rn5f107s2/Molybdenum/tree/OCBDatagen) to generate mostly OCB fens
    
    Passed STC:
    Elo   | 8.27 +- 6.09 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=8MB
    LLR   | 2.90 (-2.25, 2.89) [0.00, 5.00]
    Games | N: 6596 W: 1822 L: 1665 D: 3109
    Penta | [112, 773, 1378, 916, 119]
    http://aytchell.eu.pythonanywhere.com/test/351/
    
    bench 5994358

--- src/nnue.cpp
+++ src/nnue.cpp
@@ -18,7 +18,7 @@
 #undef PUSHED_MACRO
 #endif
 
-#define defaultNetPath "src/Nets/data++-40.bin"
+#define defaultNetPath "src/Nets/moly_20240418.nnue"
 
 INCBIN(network, defaultNetPath);
 const Weights defaultWeights = *reinterpret_cast<const Weights*>(gnetworkData);
