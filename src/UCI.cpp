7d8ec221bdb864c8147b169798af043a2a08ac05
rn5f107s2 <clemens.lerchl@gmail.com>
2024-04-19 18:25:54 +0200

    Bump version number bench 5994358

--- src/UCI.cpp
+++ src/UCI.cpp
@@ -22,7 +22,7 @@ TuneOptions tuneOptions;
 #endif
 
 const std::string name = "Molybdenum";
-const std::string version = "3";
+const std::string version = "3.1";
 
 void uciCommunication(const std::string& in) {
     Position internalBoard;
