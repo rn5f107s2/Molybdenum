acd4d138d55167d057eb3d95426c34c7d4b3ed6e
rn5f107s2 <clemens.lerchl@gmail.com>
2024-04-26 14:22:25 +0200

    bench 5847498

--- src/search.cpp
+++ src/search.cpp
@@ -316,7 +316,11 @@ int search(int alpha, int beta, Position &pos, int depth, SearchInfo &si, Search
                 && score > singBeta) {
                 mp.setPrioMove(stack->currMove);
                 
-                if (ttScore <= alpha && ttBound == LOWER) {
+                if (   ttScore <= alpha 
+                    && ttBound == LOWER
+                    && score >= alpha
+                    && score >= ttScore) {
+                        
                     mp.setPrioMove(ttMove);
                     mp.ignoreMove(stack->currMove);
 
