09950c0732275bc62d47b549399aeaf93eb35819
rn5f107s2 <clemens.lerchl@gmail.com>
2024-04-30 16:15:12 +0200

    bench 6009417

--- src/search.cpp
+++ src/search.cpp
@@ -311,9 +311,15 @@ int search(int alpha, int beta, Position &pos, int depth, SearchInfo &si, Search
             if (score < singBeta)
                 extensions = 1;
 
-            if (   score > singBeta
-                && stack->currMove) 
-                mp.setPrioMove(stack->currMove);
+            if (score > singBeta)
+            {
+                if (stack->currMove)
+                    mp.setPrioMove(stack->currMove);
+
+                if (   ttScore <= alpha
+                    && ttDepth > depth)
+                    continue;
+            }
         }
 
         prefetchTTEntry(pos, pc, from, to, capture);
