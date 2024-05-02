1c8667ecd00a73fcfd185844f6da81ae5fe003ee
rn5f107s2 <121191597+rn5f107s2@users.noreply.github.com>
2024-05-02 12:57:05 +0200

    Net with additional ~100m fens (#80)
    ~20m generated using the OCB branch, the other ~80m using an older version (commit 6ed2bb8)
    
    OCB fens passed fixed nodes vs master:
    Elo   | 8.33 +- 6.05 (95%)
    SPRT  | N=25000 Threads=1 Hash=8MB
    LLR   | 2.95 (-2.25, 2.89) [0.00, 5.00]
    Games | N: 8264 W: 2801 L: 2603 D: 2860
    Penta | [328, 860, 1599, 976, 369]
    http://aytchell.eu.pythonanywhere.com/test/364/
    
    Old fens passed fixed nodes vs OCB fens:
    Elo   | 10.19 +- 6.99 (95%)
    SPRT  | N=25000 Threads=1 Hash=8MB
    LLR   | 2.90 (-2.25, 2.89) [0.00, 5.00]
    Games | N: 6344 W: 2217 L: 2031 D: 2096
    Penta | [261, 615, 1280, 709, 307]
    http://aytchell.eu.pythonanywhere.com/test/365/
    
    Combinded passed STC vs master:
    Elo   | 13.33 +- 8.20 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=8MB
    LLR   | 2.91 (-2.25, 2.89) [0.00, 5.00]
    Games | N: 3808 W: 1126 L: 980 D: 1702
    Penta | [66, 409, 830, 511, 88]
    http://aytchell.eu.pythonanywhere.com/test/366/
    
    bench 7598421

--- src/nnue.cpp
+++ src/nnue.cpp
@@ -18,7 +18,7 @@
 #undef PUSHED_MACRO
 #endif
 
-#define defaultNetPath "src/Nets/moly_20240418.nnue"
+#define defaultNetPath "src/Nets/moly_20240501.nnue"
 
 INCBIN(network, defaultNetPath);
 const Weights defaultWeights = *reinterpret_cast<const Weights*>(gnetworkData);
