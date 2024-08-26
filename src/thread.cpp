c0d7a0b80590ece1e44ff6cab7dc9b6689d49eb1
rn5f107s2 <121191597+rn5f107s2@users.noreply.github.com>
2024-08-26 09:19:06 +0200

    Try to make windows defender happy

--- src/thread.cpp
+++ src/thread.cpp
@@ -24,7 +24,7 @@ void Thread::initSearchInfo(SearchTime st) {
 }
 
 void Thread::detach() {
-    while (!thread.joinable()) {}
+    while (!thread.joinable()) { if (thread.joinable()) break; }
 
     thread.detach();
 }
