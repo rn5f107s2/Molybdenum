0ee63b291fc0734cafe5328f2341fa1153ed5cee
rn5f107s2 <clemens.lerchl@gmail.com>
2026-02-02 10:46:34 +0100

    bench 24964

--- src/search.cpp
+++ src/search.cpp
@@ -111,7 +111,7 @@ int SearchState::iterativeDeepening(Position  &pos, SearchTime &st, int maxDepth
             std::cout << uciOutput << std::endl;
         }
 
-        if (stop<Soft>(st, si) || thread->threads->nodes() >= 5000) {
+        if (stop<Soft>(st, si) || thread->threads->nodes() >= 1000) {
             thread->threads->stop();
             break;
         }
